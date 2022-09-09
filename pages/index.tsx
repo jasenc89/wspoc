9f51c5c15271d4bf859565ac6f62a336de130b39
Jasen Chawda <jasen.chawda@navos.co.uk>
2022-09-09 13:32:28 +0100

    styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>hello</div>;
+  return (
+    <div className="py-16 bg-black text-white h-screen">
+      <h1 className="text-center text-xl font-bold">Web Socket Demo</h1>
+      <div className="flex items-center justify-center py-12">
+        <h2>Data Output</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
